b35c8f50507321ed27940240ec839d5f81d99e3b
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-13 03:43:13 +0530

    update index in Front end,Add reg_user entity and dto

---
+++ Back_End/src/main/java/com/easy/car_rental/entity/Reg_User.java
@@ -0,0 +1,20 @@
+package com.easy.car_rental.entity;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class Reg_User {
+    String user_Id;
+    String role_Type;
+    String name;
+    String contact_No;
+    String address;
+    String email;
+    String nic;
+    String license_No;
+    String password;
+    String nic_Img;
+    String license_Img;
+
+}
