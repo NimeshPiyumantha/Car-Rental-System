7b840103369be35eac81bdb4a3fa16f06b8cfb0c
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-13 22:43:22 +0530

    update  back end,Add admin DTO

---
+++ Back_End/src/main/java/com/easy/car_rental/dto/AdminDTO.java
@@ -0,0 +1,32 @@
+package com.easy.car_rental.dto;
+
+import com.easy.car_rental.embeded.Name;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class AdminDTO {
+    private String admin_Id;
+    private String role_Type;
+    @Embedded
+    private Name name;
+    private String contact_No;
+    private String address;
+    private String email;
+    private String nic;
+    private String password;
+
+}
