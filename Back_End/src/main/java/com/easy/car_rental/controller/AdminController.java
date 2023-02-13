19cd5ddd63e53ec2263ecac12d1635d3d80dca77
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-14 01:11:55 +0530

    update back end,Add Controller package to Admin Controller

---
+++ Back_End/src/main/java/com/easy/car_rental/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.easy.car_rental.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@RestController
+@CrossOrigin
+@RequestMapping("/Admin")
+public class AdminController {
+}
