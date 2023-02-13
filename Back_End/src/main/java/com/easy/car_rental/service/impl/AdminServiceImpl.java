d36570ec44664f742fb34c208c3fe934c1292868
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-14 00:44:00 +0530

    update back end,Add Service pakage impl package to AdminServiceImpl class

--- Back_End/src/main/java/com/easy/car_rental/service/impl/AdminServiceImpl.java
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/AdminServiceImpl.java
@@ -1,7 +1,11 @@
 package com.easy.car_rental.service.impl;
 
+import com.easy.car_rental.dto.Reg_UserDTO;
+import com.easy.car_rental.service.AdminService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 
 /**
  * @author : Nimesh Piyumantha
@@ -9,5 +13,24 @@ import org.springframework.transaction.annotation.Transactional;
  **/
 @Service
 @Transactional
-public class AdminServiceImpl {
+public class AdminServiceImpl implements AdminService {
+    @Override
+    public void saveAdmin(Reg_UserDTO dto) {
+
+    }
+
+    @Override
+    public void updateAdmin(Reg_UserDTO dto) {
+
+    }
+
+    @Override
+    public void deleteUser(String reg_ID) {
+
+    }
+
+    @Override
+    public ArrayList<Reg_UserDTO> getAllAdmin() {
+        return null;
+    }
 }
