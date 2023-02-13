e2dcea0e66254b60e714361389833ba433e5c7a0
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-13 15:36:25 +0530

    update index in Front end,contact From

--- Back_End/src/main/java/com/easy/car_rental/dto/Reg_UserDTO.java
+++ Back_End/src/main/java/com/easy/car_rental/dto/Reg_UserDTO.java
@@ -1,9 +1,12 @@
 package com.easy.car_rental.dto;
 
+import com.easy.car_rental.embeded.Name;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import javax.persistence.Embedded;
 
 /**
  * @author : Nimesh Piyumantha
@@ -16,7 +19,8 @@ import lombok.ToString;
 public class Reg_UserDTO {
     private String user_Id;
     private String role_Type;
-    private String name;
+    @Embedded
+    private Name name;
     private String contact_No;
     private String address;
     private String email;
