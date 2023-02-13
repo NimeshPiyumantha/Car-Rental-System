ebed965aa64fa94bbf8d5062b348b8879a4bd038
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-14 00:29:47 +0530

    update back end,Add Service pakage to Admin Service saveAdmin

---
+++ Back_End/src/main/java/com/easy/car_rental/service/AdminService.java
@@ -0,0 +1,11 @@
+package com.easy.car_rental.service;
+
+import com.easy.car_rental.dto.Reg_UserDTO;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public interface AdminService {
+    void saveAdmin(Reg_UserDTO dto);
+}
