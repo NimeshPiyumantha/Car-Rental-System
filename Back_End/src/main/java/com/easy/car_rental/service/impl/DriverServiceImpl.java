c1f7547169fade71f8682caa9f64847ccb40e736
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-14 10:21:19 +0530

    update back end,Add Service impl package to driverService class driver save

--- Back_End/src/main/java/com/easy/car_rental/service/impl/DriverServiceImpl.java
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/DriverServiceImpl.java
@@ -1,5 +1,8 @@
 package com.easy.car_rental.service.impl;
 
+import com.easy.car_rental.dto.DriverDTO;
+import com.easy.car_rental.entity.Admin;
+import com.easy.car_rental.entity.Driver;
 import com.easy.car_rental.repo.AdminRepo;
 import com.easy.car_rental.repo.DriverRepo;
 import com.easy.car_rental.service.DriverService;
@@ -21,6 +24,12 @@ public class DriverServiceImpl implements DriverService {
 
     @Autowired
     private ModelMapper mapper;
-}
-}
 
+    @Override
+    public void saveDriver(DriverDTO dto) {
+        if (repo.existsById(dto.getDriver_Id())) {
+            throw new RuntimeException("Admin Already Exist. Please enter another id..!");
+        }
+        repo.save(mapper.map(dto, Driver.class));
+    }
+}
