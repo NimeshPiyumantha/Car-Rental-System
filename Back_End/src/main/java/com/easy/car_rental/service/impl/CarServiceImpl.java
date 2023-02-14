6e42c64f9b1548beea3b5758f032dc52981eef5d
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-14 10:54:19 +0530

    update back end,Add service package impl pakage to CarServiceImpl implement CarService car repo object

--- Back_End/src/main/java/com/easy/car_rental/service/impl/CarServiceImpl.java
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/CarServiceImpl.java
@@ -1,10 +1,19 @@
 package com.easy.car_rental.service.impl;
 
+import com.easy.car_rental.dto.CarDTO;
+import com.easy.car_rental.repo.CarRepo;
+import com.easy.car_rental.repo.DriverRepo;
 import com.easy.car_rental.service.CarService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public class CarServiceImpl implements CarService {
+    @Autowired
+    private CarRepo repo;
+
+
 }
