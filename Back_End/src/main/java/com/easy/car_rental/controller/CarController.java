736027de99878acd22f51a2fe8dd8e5d7ca0873b
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-14 11:02:40 +0530

    update back end,Add controller package to Controller class car save

--- Back_End/src/main/java/com/easy/car_rental/controller/CarController.java
+++ Back_End/src/main/java/com/easy/car_rental/controller/CarController.java
@@ -1,11 +1,14 @@
 package com.easy.car_rental.controller;
 
+import com.easy.car_rental.dto.CarDTO;
+import com.easy.car_rental.dto.DriverDTO;
 import com.easy.car_rental.service.CarService;
 import com.easy.car_rental.service.DriverService;
+import com.easy.car_rental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author : Nimesh Piyumantha
@@ -17,4 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CarController {
     @Autowired
     private CarService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveCar(@RequestBody CarDTO dto) {
+        service.saveCar(dto);
+        System.out.println(dto);
+        return new ResponseUtil("OK", "Successfully Registered.!", null);
+    }
 }
