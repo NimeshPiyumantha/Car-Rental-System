a0639dd8589bfbaf73e9081dd00498ea5727377e
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-14 09:32:51 +0530

    update back end,Add Service to Driver Service Driver update

--- Back_End/src/main/java/com/easy/car_rental/service/DriverService.java
+++ Back_End/src/main/java/com/easy/car_rental/service/DriverService.java
@@ -1,6 +1,9 @@
 package com.easy.car_rental.service;
 
+import com.easy.car_rental.dto.AdminDTO;
 import com.easy.car_rental.dto.DriverDTO;
+
+import java.util.ArrayList;
 
 /**
  * @author : Nimesh Piyumantha
@@ -8,5 +11,6 @@ import com.easy.car_rental.dto.DriverDTO;
  **/
 public interface DriverService {
     void saveDriver(DriverDTO dto);
+    void updateDriver(DriverDTO dto);
 
 }
