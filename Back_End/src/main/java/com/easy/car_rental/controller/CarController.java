3790275c71d49e54b9710beae6bfc8455239de7d
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-14 11:06:05 +0530

    update back end,Add controller package to Controller class car get all

--- Back_End/src/main/java/com/easy/car_rental/controller/CarController.java
+++ Back_End/src/main/java/com/easy/car_rental/controller/CarController.java
@@ -38,8 +38,14 @@ public class CarController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @DeleteMapping(params = {"car_Id"})
-    public ResponseUtil deleteDriver(@RequestParam String car_Id) {
+    public ResponseUtil deleteCar(@RequestParam String car_Id) {
         service.deleteCar(car_Id);
         return new ResponseUtil("OK", "Successfully Deleted. :" + car_Id, null);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping
+    public ResponseUtil getAllCar() {
+        return new ResponseUtil("OK", "Successfully Loaded. :", service.getAllCar());
     }
 }
