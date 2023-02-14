d0fcc1da64db79ecab087843353cbd0faa8d4ddd
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-14 10:46:49 +0530

    update back end,Add service package to Car Service car save

--- Back_End/src/main/java/com/easy/car_rental/service/CarService.java
+++ Back_End/src/main/java/com/easy/car_rental/service/CarService.java
@@ -1,8 +1,12 @@
 package com.easy.car_rental.service;
 
+import com.easy.car_rental.dto.AdminDTO;
+import com.easy.car_rental.dto.CarDTO;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public interface CarService {
+    void saveCar(CarDTO dto);
 }
