a14554b07c22d1edd7e2d2deba756255678a16e6
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-14 09:35:47 +0530

    update back end,Add Service impl package to driverService class

---
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/DriverServiceImpl.java
@@ -0,0 +1,13 @@
+package com.easy.car_rental.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@Service
+@Transactional
+public class DriverServiceImpl {
+}
