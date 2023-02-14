6cff8c5d99652df968a27c6fb01ad516363c7153
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-14 09:29:57 +0530

    update back end,Add repo Package to Driver Repo

---
+++ Back_End/src/main/java/com/easy/car_rental/repo/DriverRepo.java
@@ -0,0 +1,12 @@
+package com.easy.car_rental.repo;
+
+import com.easy.car_rental.entity.Admin;
+import com.easy.car_rental.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public interface DriverRepo extends JpaRepository<Driver, String> {
+}
