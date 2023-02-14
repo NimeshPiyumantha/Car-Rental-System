749d4457dcaee1b8cbdc5e716a1bba8fd5314f61
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-14 10:53:13 +0530

    update back end,Add service package impl pakage to CarServiceImpl implement CarService

--- Back_End/src/main/java/com/easy/car_rental/service/impl/CarServiceImpl.java
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/CarServiceImpl.java
@@ -1,8 +1,10 @@
 package com.easy.car_rental.service.impl;
 
+import com.easy.car_rental.service.CarService;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
-public class CarServiceImpl {
+public class CarServiceImpl implements CarService {
 }
