ea2727e91701a6e5ead88e1790920bec8d556aa5
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-16 00:17:45 +0530

    update front ,Add util pakage

--- Front_End/controller/Reg_UserController.js
+++ Front_End/controller/Reg_UserController.js
@@ -2,6 +2,7 @@
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
+/*Save Users*/
 let baseUrl = "http://localhost:8080/Back_End_war/";
 $("#btnSaveCustomer").click(function () {
     let formData = $("#customerForm").serialize();
@@ -12,8 +13,37 @@ $("#btnSaveCustomer").click(function () {
         data: formData,
         success: function (res) {
             saveUpdateAlert("User", res.message);
+            generateCustomerID();
         }, error: function (error) {
             unSuccessUpdateAlert("User", JSON.parse(error.responseText).message);
         }
     });
 });
+
+
+/* User Id Gentrator */
+function generateCustomerID() {
+    $("#txtCusId").val("C00-001");
+    $.ajax({
+        url: baseUrl + "reg_User/reg_UserIdGenerate",
+        method: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (resp) {
+            let id = resp.value;
+            console.log("id" +id);
+            let tempId = parseInt(id.split("-")[1]);
+            tempId = tempId + 1;
+            if (tempId <= 9) {
+                $("#txtCusId").val("C00-00" + tempId);
+            } else if (tempId <= 99) {
+                $("#txtCusId").val("C00-0" + tempId);
+            } else {
+                $("#txtCusId").val("C00-" + tempId);
+            }
+        },
+        error: function (ob, statusText, error) {
+
+        }
+    });
+}
