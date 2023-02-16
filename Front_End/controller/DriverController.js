badb703e6925f1eb8d91ea55e1f01eef05405b22
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-17 02:30:03 +0530

    update front end,Add front driver controller pakage to validations

--- Front_End/controller/DriverController.js
+++ Front_End/controller/DriverController.js
@@ -72,3 +72,132 @@ function setTextFieldValues(firstName, lastName, contact_No, address, email, nic
     $("#btnSaveCustomer").attr('disabled', true);
 }
 
+/**
+ * Auto Forces Input Fields Save
+ * */
+$("#firstName").focus();
+const regExFirstName = /^[A-z ]{3,20}$/;
+const regExLastName = /^[A-z ]{3,20}$/;
+const regExContactNum = /^(07(0|1|2|4|5|6|7|8)[0-9]{7})$/;
+const regExCusAddress = /^[A-z0-9/ ]{4,30}$/;
+const regExEmailCusAddress = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const regExNIC = /^([0-9]{12}|[0-9V]{10})$/;
+const regExDrivingNIC = /^[A-Z0-9-]+$/;
+const regExUserName = /^[A-z0-9/ ]{4,30}$/;
+const regExPassword = /^([A-Z a-z]{5,15}[0-9]{1,10})$/;
+
+let customerValidations = [];
+customerValidations.push({
+    reg: regExFirstName, field: $('#firstName'), error: 'User First Name Pattern is Wrong'
+});
+customerValidations.push({
+    reg: regExLastName, field: $('#lastName'), error: 'User Last Name Pattern is Wrong'
+});
+customerValidations.push({
+    reg: regExContactNum, field: $('#contact_No'), error: 'User Contact Number Pattern is Wrong'
+});
+customerValidations.push({
+    reg: regExCusAddress, field: $('#address'), error: 'User Address Pattern is Wrong'
+});
+customerValidations.push({
+    reg: regExEmailCusAddress, field: $('#email'), error: 'User Email Address Pattern is Wrong'
+});
+customerValidations.push({
+    reg: regExNIC, field: $('#nic_No'), error: 'User NIC Pattern is Wrong'
+});
+customerValidations.push({
+    reg: regExDrivingNIC, field: $('#license_No'), error: 'User Driving License Pattern is Wrong'
+});
+customerValidations.push({
+    reg: regExUserName, field: $('#user_Name'), error: 'User User Name Pattern is Wrong'
+});
+customerValidations.push({
+    reg: regExPassword, field: $('#password'), error: 'User Password Pattern is Wrong'
+});
+//disable tab key of all four text fields using grouping selector in CSS
+$("#firstName,#lastName,#contact_No,#address,#email,#nic_No,#license_No,#user_Name,#password").on('keydown', function (event) {
+    if (event.key === "Tab") {
+        event.preventDefault();
+    }
+});
+
+$("#firstName,#lastName,#contact_No,#address,#email,#nic_No,#license_No,#user_Name,#password").on('keyup', function (event) {
+    checkValidity(customerValidations);
+});
+
+$("#firstName,#lastName,#contact_No,#address,#email,#nic_No,#license_No,#user_Name,#password").on('blur', function (event) {
+    checkValidity(customerValidations);
+});
+
+$("#firstName").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExFirstName, $("#firstName"))) {
+        $("#lastName").focus();
+    } else {
+        focusText($("#firstName"));
+    }
+});
+
+$("#lastName").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExLastName, $("#lastName"))) {
+        focusText($("#contact_No"));
+    }
+});
+
+$("#contact_No").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExContactNum, $("#contact_No"))) {
+        focusText($("#address"));
+    }
+});
+
+$("#address").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExCusAddress, $("#address"))) {
+        if (event.which === 13) {
+            focusText($("#email"));
+        }
+    }
+});
+
+$("#email").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExEmailCusAddress, $("#email"))) {
+        focusText($("#nic_No"));
+    }
+});
+
+$("#nic_No").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExNIC, $("#nic_No"))) {
+        focusText($("#license_No"));
+    }
+});
+
+$("#license_No").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExDrivingNIC, $("#license_No"))) {
+        if (event.which === 13) {
+            focusText($("#user_Name"));
+        }
+    }
+});
+
+$("#user_Name").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExUserName, $("#user_Name"))) {
+        if (event.which === 13) {
+            focusText($("#password"));
+        }
+    }
+});
+
+$("#password").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExPassword, $("#password"))) {
+        if (event.which === 13) {
+            $('#btnSaveCustomer').focus();
+        }
+    }
+});
+
+
+function setButtonState(value) {
+    if (value > 0) {
+        $("#btnSaveDriver").attr('disabled', true);
+    } else {
+        $("#btnSaveDriver").attr('disabled', false);
+    }
+}
