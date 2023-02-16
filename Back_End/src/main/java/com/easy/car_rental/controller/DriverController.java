960429df33d2e5798a710cdf72b12855f29c60b9
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-17 02:35:10 +0530

    update back end,Add Driver save

--- Back_End/src/main/java/com/easy/car_rental/controller/DriverController.java
+++ Back_End/src/main/java/com/easy/car_rental/controller/DriverController.java
@@ -1,6 +1,8 @@
 package com.easy.car_rental.controller;
 
 import com.easy.car_rental.dto.DriverDTO;
+import com.easy.car_rental.dto.UserDTO;
+import com.easy.car_rental.embeded.Name;
 import com.easy.car_rental.service.DriverService;
 import com.easy.car_rental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +23,11 @@ public class DriverController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping()
-    public ResponseUtil saveDriver(@RequestBody DriverDTO dto) {
-        service.saveDriver(dto);
-        System.out.println(dto);
+    public ResponseUtil saveDriver(@ModelAttribute DriverDTO driverDTO, @ModelAttribute UserDTO userDTO, @ModelAttribute Name name) {
+        driverDTO.setName(name);
+        driverDTO.setUserDTO(userDTO);
+        System.out.println(driverDTO);
+        service.saveDriver(driverDTO);
         return new ResponseUtil("OK", "Successfully Registered.!", null);
     }
 
