9f4a4093827657749bfe97d620621779a8102756
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-17 14:42:55 +0530

    update front end,Add front Car controller

---
+++ Front_End/controller/CarController.js
@@ -0,0 +1,27 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+/*Save Driver*/
+let baseUrl = "http://localhost:8080/Back_End_war/";
+
+// $("#btnSaveDriver").attr('disabled', true);
+
+$("#btnSaveCar").click(function () {
+    let formData = new FormData($("#carForm")[0]);
+    console.log(formData);
+    $.ajax({
+        url: baseUrl + "car",
+        method: "post",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (res) {
+            console.log(res)
+            saveUpdateAlert("Car", res.message);
+        },
+        error: function (error) {
+            unSuccessUpdateAlert("Car", JSON.parse(error.responseText).message);
+        }
+    });
+});
