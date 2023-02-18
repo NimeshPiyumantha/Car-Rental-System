e7d4bc7073eb8af38bbb41f4b514b53cf5f87a6b
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-18 21:16:24 +0530

    Update Back End,Add Database DriverSchedule Entity

---
+++ Back_End/src/main/java/com/easy/car_rental/entity/DriverSchedule_PK.java
@@ -0,0 +1,12 @@
+package com.easy.car_rental.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class DriverSchedule_PK implements Serializable {
+    private String driver_Id;
+    private String rent_Id;
+}
