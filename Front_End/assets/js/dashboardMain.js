95906de35f109d8d223a34cc0644b35c2f95f839
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-18 06:44:00 +0530

    Add front End Dashboard  to Back to top button js

--- Front_End/assets/js/dashboardMain.js
+++ Front_End/assets/js/dashboardMain.js
@@ -81,4 +81,19 @@
         onscroll(document, headerScrolled)
     }
 
+    /**
+     * Back to top button
+     */
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
 })
