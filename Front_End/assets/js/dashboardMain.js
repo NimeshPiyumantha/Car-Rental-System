b0072f85f70ac9e99d363da56fb9223eb59aced2
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-18 06:33:52 +0530

    Add front End Dashboard  to Easy event listener function js

--- Front_End/assets/js/dashboardMain.js
+++ Front_End/assets/js/dashboardMain.js
@@ -2,3 +2,30 @@
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
+
+(function() {
+    "use strict";
+
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+        if (all) {
+            select(el, all).forEach(e => e.addEventListener(type, listener))
+        } else {
+            select(el, all).addEventListener(type, listener)
+        }
+    }
+
