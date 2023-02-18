4559d0e89bf778d1649d1dfbc11bb9021ed34b7b
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-18 21:18:14 +0530

    Update Back End,Add Database DriverSchedule Entity

---
+++ Back_End/src/main/java/com/easy/car_rental/entity/Driver_Schedule.java
@@ -0,0 +1,33 @@
+package com.easy.car_rental.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+@Entity
+@IdClass(DriverSchedule_PK.class)
+public class Driver_Schedule {
+    @Id
+    private String driver_Id;
+    @Id
+    private String rent_Id;
+
+    @ManyToOne
+    @JoinColumn(name = "driver_Id",referencedColumnName = "user_Id",insertable = false,updatable = false)
+    private Driver driver;
+
+    @ManyToOne
+    @JoinColumn(name = "rent_Id",referencedColumnName = "rent_Id",insertable = false,updatable = false)
+    private Rent rent;
+}
