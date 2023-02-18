71c325ffdb1409d44c0b922f1eab9b352059b298
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-18 13:07:27 +0530

    Add Back end,Add Car Id Gentarte

--- Back_End/src/main/java/com/easy/car_rental/service/CarService.java
+++ Back_End/src/main/java/com/easy/car_rental/service/CarService.java
@@ -1,6 +1,7 @@
 package com.easy.car_rental.service;
 
 import com.easy.car_rental.dto.CarDTO;
+import com.easy.car_rental.dto.CustomDTO;
 
 import java.util.ArrayList;
 
@@ -10,10 +11,8 @@ import java.util.ArrayList;
  **/
 public interface CarService {
     void saveCar(CarDTO dto);
-
     void updateCar(CarDTO dto);
-
     void deleteCar(String car_Id);
-
     ArrayList<CarDTO> getAllCar();
+    CustomDTO carIdGenerate();
 }
