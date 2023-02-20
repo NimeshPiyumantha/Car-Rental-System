6a24f1943d4db9f4696efce91ee2e454c4f33c49
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-20 05:51:14 +0530

    Update Back End,Car Service

--- Back_End/src/main/java/com/easy/car_rental/service/impl/CarServiceImpl.java
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/CarServiceImpl.java
@@ -42,38 +42,23 @@ public class CarServiceImpl implements CarService {
         if (repo.existsById(dto.getCar_Id())) {
             throw new RuntimeException("Car Already Exist. Please enter another id..!");
         }
-        System.out.println(car.getImage().getFront_View());
+
         try {
-            byte[] bytes1 = dto.getImage().getFront_View().getBytes();
-            byte[] bytes2 = dto.getImage().getBack_View().getBytes();
-            byte[] bytes3 = dto.getImage().getSide_View().getBytes();
-            byte[] bytes4 = dto.getImage().getInterior().getBytes();
 
             String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
             File uploadsDir = new File(projectPath + "/uploads");
             System.out.println(projectPath);
-
             uploadsDir.mkdir();
 
-            Path location1 = Paths.get(uploadsDir + "/car" + car.getImage().getFront_View() + ".png");
-            Path location2 = Paths.get(uploadsDir + "/car" + car.getImage().getBack_View() + ".png");
-            Path location3 = Paths.get(uploadsDir + "/car" + car.getImage().getSide_View() + ".png");
-            Path location4 = Paths.get(uploadsDir + "/car" + car.getImage().getInterior() + ".png");
+            dto.getImage().getFront_View().transferTo(new File(uploadsDir.getAbsolutePath() + "/" + dto.getImage().getFront_View().getOriginalFilename()));
+            dto.getImage().getBack_View().transferTo(new File(uploadsDir.getAbsolutePath() + "/" + dto.getImage().getBack_View().getOriginalFilename()));
+            dto.getImage().getSide_View().transferTo(new File(uploadsDir.getAbsolutePath() + "/" + dto.getImage().getSide_View().getOriginalFilename()));
+            dto.getImage().getInterior().transferTo(new File(uploadsDir.getAbsolutePath() + "/" + dto.getImage().getInterior().getOriginalFilename()));
 
-            Files.write(location1, bytes1);
-            Files.write(location2, bytes2);
-            Files.write(location3, bytes3);
-            Files.write(location4, bytes4);
-
-            dto.getImage().getFront_View().transferTo(location1);
-            dto.getImage().getBack_View().transferTo(location2);
-            dto.getImage().getSide_View().transferTo(location3);
-            dto.getImage().getInterior().transferTo(location4);
-
-            car.getImage().setFront_View(location1.toString());
-            car.getImage().setBack_View(location2.toString());
-            car.getImage().setSide_View(location3.toString());
-            car.getImage().setInterior(location4.toString());
+            car.getImage().setFront_View("uploads/"+dto.getImage().getFront_View().getOriginalFilename());
+            car.getImage().setBack_View("uploads/"+dto.getImage().getBack_View().getOriginalFilename());
+            car.getImage().setSide_View("uploads/"+dto.getImage().getSide_View().getOriginalFilename());
+            car.getImage().setInterior("uploads/"+dto.getImage().getInterior().getOriginalFilename());
 
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
@@ -85,10 +70,33 @@ public class CarServiceImpl implements CarService {
 
     @Override
     public void updateCar(CarDTO dto) {
+        Car car = new Car(dto.getCar_Id(), dto.getName(), dto.getBrand(), CarType.valueOf(dto.getType()), new Image(), dto.getNumber_Of_Passengers(), TransmissionType.valueOf(dto.getTransmission_Type()), FuelType.valueOf(dto.getFuel_Type()), dto.getRent_Duration_Price(), dto.getPrice_Extra_KM(), dto.getRegistration_Number(), dto.getFree_Mileage(), dto.getColor(), dto.getVehicleAvailabilityType());
         if (!repo.existsById(dto.getCar_Id())) {
             throw new RuntimeException("Car Not Exist. Please enter Valid id..!");
         }
-        repo.save(mapper.map(dto, Car.class));
+        try {
+
+            String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
+            File uploadsDir = new File(projectPath + "/uploads");
+            System.out.println(projectPath);
+            uploadsDir.mkdir();
+
+            dto.getImage().getFront_View().transferTo(new File(uploadsDir.getAbsolutePath() + "/" + dto.getImage().getFront_View().getOriginalFilename()));
+            dto.getImage().getBack_View().transferTo(new File(uploadsDir.getAbsolutePath() + "/" + dto.getImage().getBack_View().getOriginalFilename()));
+            dto.getImage().getSide_View().transferTo(new File(uploadsDir.getAbsolutePath() + "/" + dto.getImage().getSide_View().getOriginalFilename()));
+            dto.getImage().getInterior().transferTo(new File(uploadsDir.getAbsolutePath() + "/" + dto.getImage().getInterior().getOriginalFilename()));
+
+            car.getImage().setFront_View("uploads/"+dto.getImage().getFront_View().getOriginalFilename());
+            car.getImage().setBack_View("uploads/"+dto.getImage().getBack_View().getOriginalFilename());
+            car.getImage().setSide_View("uploads/"+dto.getImage().getSide_View().getOriginalFilename());
+            car.getImage().setInterior("uploads/"+dto.getImage().getInterior().getOriginalFilename());
+
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(car);
+        repo.save(car);
     }
 
     @Override
