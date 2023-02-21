8b7560f7dcdf185e98e6f5bf6271f72587ae69b5
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-22 01:38:45 +0530

    Update Front End,AdminLoginfrom

--- Front_End/controller/loginForm.js
+++ Front_End/controller/loginForm.js
@@ -3,38 +3,12 @@
  * @since : 0.1.0
  **/
 
-let baseUrl = "http://localhost:8080/Back_End_war/";
+let baseUrlLogin = "http://localhost:8080/Back_End_war/";
 
 
 /**
  * load all customers Method
  * */
-$("#btnLogin").click(function () {
-    let role_Type = $('#role_Type').val();
-    let user_Name = $('#user_Name').val();
-    let password = $('#password').val();
-
-    $.ajax({
-        url: baseUrl + "loginForm", method: "GET", contentType: "application/json", success: function (res) {
-
-            for (let login of res.data) {
-                    if (role_Type === login.role_Type && user_Name.trim() === login.user_Name && password.trim() === login.password) {
-                        console.log(role_Type);
-                        console.log(user_Name);
-                        console.log(password);
-
-                    } else {
-
-                    }
-            }
-
-        }, error: function (error) {
-            let message = JSON.parse(error.responseText).message;
-            console.log(message);
-        }
-
-    });
-});
 
 $("#btnLogin").on('click', function () {
     login();
@@ -46,7 +20,7 @@ function login() {
     let password = $('#password').val();
 
     $.ajax({
-        url: baseUrl + "loginForm",
+        url: baseUrlLogin + "loginForm",
         contentType: "application/json",
         dataType: "json",
         success: function (res) {
