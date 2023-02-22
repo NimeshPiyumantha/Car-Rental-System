ea5a8689a6fd26871729b2179679f4383cc87d8a
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-22 08:52:53 +0530

    Add back end RentRepo

--- Back_End/src/main/java/com/easy/car_rental/repo/RentRepo.java
+++ Back_End/src/main/java/com/easy/car_rental/repo/RentRepo.java
@@ -2,10 +2,13 @@ package com.easy.car_rental.repo;
 
 import com.easy.car_rental.entity.Rent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public interface RentRepo extends JpaRepository<Rent, String> {
+    @Query(value = "SELECT rent_Id FROM Rent ORDER BY rent_Id DESC LIMIT 1", nativeQuery = true)
+    String getLastIndex();
 }
