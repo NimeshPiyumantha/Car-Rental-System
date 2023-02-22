83ccbc11250f551ad5232abaef282762c4ef2786
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-22 08:49:50 +0530

    Add back end serviceImpl pakage to Rentserviceimpl class

---
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/RentServiceImpl.java
@@ -0,0 +1,15 @@
+package com.easy.car_rental.service.impl;
+
+import com.easy.car_rental.dto.CustomDTO;
+import com.easy.car_rental.service.RentService;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class RentServiceImpl implements RentService {
+    @Override
+    public CustomDTO rentIdGenerate() {
+        return null;
+    }
+}
