5f6f3955af17e9ee1d6a976672f36840cec6706f
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-22 09:02:33 +0530

    Add back end RentController

--- Back_End/src/main/java/com/easy/car_rental/controller/RentController.java
+++ Back_End/src/main/java/com/easy/car_rental/controller/RentController.java
@@ -1,11 +1,11 @@
 package com.easy.car_rental.controller;
 
+import com.easy.car_rental.dto.CustomDTO;
 import com.easy.car_rental.service.Reg_UserService;
 import com.easy.car_rental.service.RentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author : Nimesh Piyumantha
@@ -17,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class RentController {
     @Autowired
     private RentService service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/rentIdGenerate")
+    public @ResponseBody CustomDTO customerIdGenerate() {
+        return service.rentIdGenerate();
+    }
 }
