9b289d569acdea986e982773244a53a15421840a
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-22 08:38:22 +0530

    Add front end Rent contorller generateRentID

--- Front_End/controller/RentController.js
+++ Front_End/controller/RentController.js
@@ -4,3 +4,32 @@
  **/
 
 let baseUrl = "http://localhost:8080/Back_End_war/";
+
+/**
+ * User Id Generator
+ * */
+generateRentID();
+function generateRentID() {
+    $("#user_Id").val("REN-001");
+    $.ajax({
+        url: baseUrl + "rent/rentIdGenerate",
+        method: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (resp) {
+            let id = resp.value;
+            console.log("id" + id);
+            let tempId = parseInt(id.split("-")[1]);
+            tempId = tempId + 1;
+            if (tempId <= 9) {
+                $("#user_Id").val("REN-00" + tempId);
+            } else if (tempId <= 99) {
+                $("#user_Id").val("REN-0" + tempId);
+            } else {
+                $("#user_Id").val("REN-" + tempId);
+            }
+        },
+        error: function (ob, statusText, error) {
+        }
+    });
+}
