096b24bda2534b0c67a2a85d33326d1638c7d7df
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-22 08:51:14 +0530

    Add back end serviceImpl pakage to Rentserviceimpl class

--- Back_End/src/main/java/com/easy/car_rental/service/impl/RentServiceImpl.java
+++ Back_End/src/main/java/com/easy/car_rental/service/impl/RentServiceImpl.java
@@ -1,13 +1,25 @@
 package com.easy.car_rental.service.impl;
 
 import com.easy.car_rental.dto.CustomDTO;
+import com.easy.car_rental.repo.Reg_UserRepo;
 import com.easy.car_rental.service.RentService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
+@Service
+@Transactional
 public class RentServiceImpl implements RentService {
+    @Autowired
+    private Reg_UserRepo repo;
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public CustomDTO rentIdGenerate() {
         return null;
