fd4f765c4295a46b01b573d36a497119ae9c69a5
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-22 08:45:50 +0530

    Add back end service pakage to Rent service

---
+++ Back_End/src/main/java/com/easy/car_rental/service/RentService.java
@@ -0,0 +1,11 @@
+package com.easy.car_rental.service;
+
+import com.easy.car_rental.dto.CustomDTO;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public interface RentService {
+    CustomDTO rentIdGenerate();
+}
