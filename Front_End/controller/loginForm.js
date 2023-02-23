0aa780f77d8085ec52a894158dd78f5e156993fd
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-23 20:24:48 +0530

    Refactor Project

--- Front_End/controller/loginForm.js
+++ Front_End/controller/loginForm.js
@@ -28,6 +28,13 @@ function login() {
             for (var login of res.data) {
                 if (loginRole_Type === login.role_Type && loginUserName === login.user_Name && loginPassword === login.password) {
                     if (loginRole_Type === "DRIVER" && loginUserName === login.user_Name && loginPassword === login.password) {
+                        $.ajax({
+                            url: baseUrlLogin + "loginForm/?username=" + loginUserName + "&password=" + loginPassword,
+                            data:res.data,
+                            method:"get",
+                            success:function (res1) {
+                            }
+                        })
                         window.location.href = 'driverDashboard.html';
                     } else if (loginRole_Type === "REGISTERED_USER" && loginUserName === login.user_Name && loginPassword === login.password) {
                         $.ajax({
@@ -36,7 +43,7 @@ function login() {
                             method:"get",
                             success:function (res1) {
                             }
-                        })
+                        });
                         window.location.href = 'reg_UserDashboard.html';
                     } else if (loginRole_Type === "ADMIN" && loginUserName === login.user_Name && loginPassword === login.password) {
                         window.location.href = 'adminDashboard.html';
@@ -47,34 +54,3 @@ function login() {
         }
     });
 }
-
-
-// function login() {
-//     let role_Type = $('#role_Type').val();
-//     let user_Name = $('#user_Name').val();
-//     let password = $('#password').val();
-//
-//     $.ajax({
-//         url: baseUrlLogin + "loginForm/?role_Type=" + role_Type + "&user_Name=" + user_Name + "&password=" + password,
-//         async:false,
-//         success: function (res) {
-//             for (var login of res) {
-//                 if (role_Type === "DRIVER" && user_Name === login.user_Name && password === login.password) {
-//                     window.location.href = 'driverDashboard.html';
-//                 } else if (role_Type === "REGISTERED_USER" && user_Name === login.user_Name && password === login.password) {
-//                     window.location.href = 'reg_UserDashboard.html';
-//                 } else if (role_Type === "ADMIN" && user_Name === login.user_Name && password === login.password) {
-//                     window.location.href = 'adminDashboard.html';
-//                 }
-//                 return;
-//             }
-//
-//            /* $.ajax({
-//                 url: baseUrlLogin + "reg_User/loginUser/?user_Name=" + user_Name,async:false,
-//                 success: function (res) {
-//                     console.log(res);
-//                 }
-//             });*/
-//         }
-//     });
-//}
