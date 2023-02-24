c3018301df6c0258c93723f57c8e2806ab076948
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-24 09:39:33 +0530

    getSumReservedCar car repo query

--- Back_End/src/main/java/com/easy/car_rental/repo/CarRepo.java
+++ Back_End/src/main/java/com/easy/car_rental/repo/CarRepo.java
@@ -20,6 +20,9 @@ public interface CarRepo extends JpaRepository<Car, String> {
     @Query(value = "SELECT COUNT(car_Id) FROM Car WHERE vehicleAvailabilityType='AVAILABLE';", nativeQuery = true)
     int getSumAvailableCar();
 
+    @Query(value = "SELECT COUNT(car_Id) FROM Car WHERE vehicleAvailabilityType='UNAVAILABLE';", nativeQuery = true)
+    int getSumReservedCar();
+
     @Query(value = "SELECT * FROM Car WHERE type =?1 and fuel_Type=?2 and vehicleAvailabilityType='AVAILABLE' ", nativeQuery = true)
     ArrayList<Car> filterCar(String type, String fuel_Type);
 
