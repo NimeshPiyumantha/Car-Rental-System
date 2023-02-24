cf6398c08862f57c089fc034c3e2428273cd03d5
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-24 09:58:40 +0530

    getSumReservedCar car controller to back end

--- Back_End/src/main/java/com/easy/car_rental/controller/CarController.java
+++ Back_End/src/main/java/com/easy/car_rental/controller/CarController.java
@@ -85,6 +85,12 @@ public class CarController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/reservedCarCount")
+    public @ResponseBody CustomDTO getSumReservedCar() {
+        return service.getSumReservedCar();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
     @GetMapping(path = "/filterCarDetails",params = {"category_type","fuel_Type"})
     public ArrayList<CarDTO> searchDriverId(@RequestParam String category_type, @RequestParam String fuel_Type) {
         System.out.println(category_type);
