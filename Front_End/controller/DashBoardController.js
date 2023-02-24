f46d55ee2a6d56685dd54b4d8490cf07c9bb3add
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-24 11:20:49 +0530

    getSumOfBookingActive front end controller

--- Front_End/controller/DashBoardController.js
+++ Front_End/controller/DashBoardController.js
@@ -172,3 +172,22 @@ $.ajax({
 
     }
 });
+
+/**
+ * Booking Active Count
+ * */
+$("#BookingActiveCount").val("00");
+$.ajax({
+    url: dashboard + "rent/bookingActive",
+    method: "GET",
+    contentType: "application/json",
+    dataType: "json",
+    success: function (resp) {
+        let num = resp.count;
+        $("#BookingActiveCount").text(num);
+
+    },
+    error: function (ob, statusText, error) {
+
+    }
+});
