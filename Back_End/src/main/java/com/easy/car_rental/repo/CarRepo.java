e61d46acf17ca7d771171294321b8ed9108291bc
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-25 10:21:15 +0530

    Add UNDER MAINTAIN car search in cars

--- Back_End/src/main/java/com/easy/car_rental/repo/CarRepo.java
+++ Back_End/src/main/java/com/easy/car_rental/repo/CarRepo.java
@@ -22,8 +22,12 @@ public interface CarRepo extends JpaRepository<Car, String> {
 
     @Query(value = "SELECT COUNT(car_Id) FROM Car WHERE vehicleAvailabilityType='UNAVAILABLE';", nativeQuery = true)
     int getSumReservedCar();
+
     @Query(value = "SELECT COUNT(car_Id) FROM Car WHERE vehicleAvailabilityType='MAINTAIN';", nativeQuery = true)
     int getSumMaintainCar();
+
+    @Query(value = "SELECT COUNT(car_Id) FROM Car WHERE vehicleAvailabilityType='UNDER_MAINTAIN';", nativeQuery = true)
+    int getSumUnderMaintainCar();
 
     @Query(value = "SELECT * FROM Car WHERE type =?1 and fuel_Type=?2 and vehicleAvailabilityType='AVAILABLE' ", nativeQuery = true)
     ArrayList<Car> filterCar(String type, String fuel_Type);
