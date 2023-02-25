2ce62892b53bd0d7a9ca34f00e124ffe4dc1dcba
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-26 01:51:08 +0530

    Refactor Project

--- Front_End/controller/RentController.js
+++ Front_End/controller/RentController.js
@@ -16,9 +16,26 @@ $.ajax({
     success: function (res) {
         console.log(res.data)
         for (var i of res.data) {
-                let row = "<tr><td>" + i.rentID + "</td><td>" + i.rentDetails.at().carID + "</td><td>" + i.regUser.user_Id + "</td><td>" + i.rentDetails.at().driverID + "</td><td>" + i.requestType + "</td><td>" + i.rentType + "</td><td>" + i.pickUpDate + "</td><td>" + i.pickUpTime + "</td><td>" + i.returnTime + "</td><td>" + i.returnDate + "</td><td>" + i.location + "</td></tr>";
-                $("#rentAllDetails").append(row);
+            let row = "<tr><td>" + i.rentID + "</td><td>" + i.rentDetails.at().carID + "</td><td>" + i.regUser.user_Id + "</td><td>" + i.rentDetails.at().driverID + "</td><td>" + i.requestType + "</td><td>" + i.rentType + "</td><td>" + i.pickUpDate + "</td><td>" + i.pickUpTime + "</td><td>" + i.returnTime + "</td><td>" + i.returnDate + "</td><td>" + i.location + "</td></tr>";
+            $("#rentAllDetails").append(row);
             $("#retManage").append(row);
+            blindClickEventsRent();
         }
     }
 });
+
+
+function blindClickEventsRent() {
+    $("#retManage>tr").on("click", function () {
+        let requestRentId = $(this).children().eq(0).text();
+        let driverId = $(this).children().eq(3).text();
+        let userID = $(this).children().eq(2).text();
+        let requestState = $(this).children().eq(5).text();
+
+        $("#requestRentId").val(requestRentId);
+        $("#driverId").val(driverId);
+        $("#userID").val(userID);
+        $("#requestState").val(requestState);
+    });
+}
+
