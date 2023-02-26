d34bec988424520328e5bc479e85df49b592ec00
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-26 13:45:48 +0530

    Refactor Project

--- Back_End/src/main/java/com/easy/car_rental/service/PaymentService.java
+++ Back_End/src/main/java/com/easy/car_rental/service/PaymentService.java
@@ -1,6 +1,8 @@
 package com.easy.car_rental.service;
 
+import com.easy.car_rental.dto.CarDTO;
 import com.easy.car_rental.dto.CustomDTO;
+import com.easy.car_rental.dto.PaymentDTO;
 
 /**
  * @author : Nimesh Piyumantha
@@ -8,4 +10,5 @@ import com.easy.car_rental.dto.CustomDTO;
  **/
 public interface PaymentService {
     CustomDTO paymentIdGenerate();
+    void savePayment(PaymentDTO dto);
 }
