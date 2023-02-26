3fc41d311d13f57a3e89322233a21ce6a85047d1
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-26 23:49:54 +0530

    Refactor Project

--- Front_End/controller/RentController.js
+++ Front_End/controller/RentController.js
@@ -221,7 +221,7 @@ $.ajax({
     success: function (res) {
         console.log(res.data);
         for (let i of res.data) {
-            let row = "<tr><td>" + i.paymentID + "</td><td>" + i.date + "</td><td>" + i.total + "</td><td>" + i.rentID.rentID + "</td><td>" + i.rentID.regUser.user_Id + "</td></tr>";
+            let row = "<tr><td>" + i.paymentID + "</td><td>" + i.rentID.rentID + "</td><td>" + i.rentID.regUser.user_Id + "</td><td>" + i.paymentType + "</td><td>" + i.date + "</td><td>" + i.time + "</td><td>" + i.total + "</td></tr>";
             $("#paymentTable").append(row);
         }
     },
