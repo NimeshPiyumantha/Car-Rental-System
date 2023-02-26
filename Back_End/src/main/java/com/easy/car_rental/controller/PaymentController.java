17c31a06faddd1b498f04b7134932036c1625827
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-26 11:23:10 +0530

    Add Back end to payemnt controller

---
+++ Back_End/src/main/java/com/easy/car_rental/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.easy.car_rental.controller;
+
+import com.easy.car_rental.dto.CustomDTO;
+import com.easy.car_rental.service.CarService;
+import com.easy.car_rental.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@RestController
+@CrossOrigin
+@RequestMapping("/payment")
+public class PaymentController {
+    @Autowired
+    private PaymentService service;
+    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping(path = "/paymentIdGenerate")
+    public @ResponseBody CustomDTO customerIdGenerate() {
+        return service.paymentIdGenerate();
+    }
+
+}
