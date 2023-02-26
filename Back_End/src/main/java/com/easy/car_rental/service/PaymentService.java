ca95c2e49a767295adfb6bc5eddd2dca288eb223
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-26 11:03:22 +0530

    Add Back end to payemnt Service

--- Back_End/src/main/java/com/easy/car_rental/service/PaymentService.java
+++ Back_End/src/main/java/com/easy/car_rental/service/PaymentService.java
@@ -1,5 +1,11 @@
-package com.easy.car_rental.service;/**
+package com.easy.car_rental.service;
+
+import com.easy.car_rental.dto.CustomDTO;
+
+/**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
- **/public interface PaymentService {
+ **/
+public interface PaymentService {
+    CustomDTO paymentIdGenerate();
 }
