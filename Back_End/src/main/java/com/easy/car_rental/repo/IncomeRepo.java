80f27bfb3db224bd79ba8cc6061cddc822da9620
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-27 09:16:16 +0530

    Add income report

--- Back_End/src/main/java/com/easy/car_rental/repo/IncomeRepo.java
+++ Back_End/src/main/java/com/easy/car_rental/repo/IncomeRepo.java
@@ -1,12 +1,20 @@
 package com.easy.car_rental.repo;
 
 import com.easy.car_rental.dto.CustomDTO;
-import com.easy.car_rental.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public interface IncomeRepo extends JpaRepository<CustomDTO, String> {
+    @Query(value = "SELECT Payment.date,count(payment.paymentID),sum(payment.total) FROM Rent INNER JOIN Payment ON Rent.rentID = Payment.rentID GROUP BY date", nativeQuery = true)
+    String dailyIncome();
+
+    @Query(value = "SELECT (MONTHNAME(date )) ,count(payment.paymentID),sum(payment.total)FROM Rent INNER JOIN Payment ON Rent.rentID = Payment.rentID  GROUP BY extract(MONTH FROM(date))", nativeQuery = true)
+    String MonthlyIncome();
+
+    @Query(value = "SELECT (YEAR(date )) ,count(payment.paymentID),sum(payment.total)FROM Rent INNER JOIN Payment ON Rent.rentID = Payment.rentID  GROUP BY extract(YEAR FROM(date))", nativeQuery = true)
+    String AnnuallyIncome();
 }
--- Back_End/src/main/java/com/easy/car_rental/repo/PaymentRepo.java
+++ Back_End/src/main/java/com/easy/car_rental/repo/PaymentRepo.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
- *
  **/
 public interface PaymentRepo extends JpaRepository<Payment, String> {
     @Query(value = "SELECT paymentID FROM Payment ORDER BY paymentID DESC LIMIT 1", nativeQuery = true)
