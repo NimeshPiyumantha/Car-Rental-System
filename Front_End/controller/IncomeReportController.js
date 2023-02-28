ba5baf4d3b79b2e88585c6125766438372285ef6
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-02-28 07:56:12 +0530

    Refactor Project

--- Front_End/controller/IncomeReportController.js
+++ Front_End/controller/IncomeReportController.js
@@ -4,42 +4,36 @@
  **/
 let baseUrlIncome = "http://localhost:8080/Back_End_war/";
 
+document.addEventListener("DOMContentLoaded", () => {
+    /**
+     * Daily Income
+     **/
+    $.ajax({
+        url: baseUrlIncome + "income/dailyIncome", method: "GET", dataType: "json", success: function (res) {
+            console.log(res);
+            for (let i = 0; i < res.length; i++) {
+                let date = res[i][0];
+                let now = new Date(date);
+                let newDate = now.getDate();
+                let newMonth = now.getMonth() + 1;
+                let newYear = now.getFullYear();
+                var formattedDate = newYear + "-" + newMonth.toString().padStart(2, "0") + "-" + newDate.toString().padStart(2, "0");
+                let count = res[i][1];
+                let total = res[i][2];
+                let row = "<tr><td>" + formattedDate + "</td><td>" + count + "</td><td>" + total + "</td></tr>";
+                $("#dailyIncomeTable").append(row);
 
-/**
- * Daily Income
- **/
-$.ajax({
-    url: baseUrlIncome + "income/dailyIncome",
-    method: "GET",
-    dataType: "json",
-    success: function (res) {
-        console.log(res);
-        for (let i=0; i<res.length; i++) {
-       let date = res[i][0];
-        let now= new Date(date);
-        let newDate = now.getDate();
-        let newMonth = now.getMonth()+1;
-        let newYear = now.getFullYear();
-        var formattedDate = newYear + "-" + newMonth.toString().padStart(2, "0") + "-" + newDate.toString().padStart(2, "0");
-            let count = res[i][1];
-            let total = res[i][2];
-            let row = "<tr><td>" + formattedDate+ "</td><td>" + count + "</td><td>" + total + "</td></tr>";
-            $("#dailyIncomeTable").append(row);
 
-            document.addEventListener("DOMContentLoaded", () => {
                 new Chart(document.querySelector('#dailyIncomeChart'), {
-                    type: 'line',
-                    data: {
-                        labels: [formattedDate],
-                        datasets: [{
+                    type: 'line', data: {
+                        labels: [formattedDate], datasets: [{
                             label: 'Daily Line Chart',
                             data: [total],
                             fill: false,
                             borderColor: 'rgb(75, 192, 192)',
                             tension: 0.1
                         }]
-                    },
-                    options: {
+                    }, options: {
                         scales: {
                             y: {
                                 beginAtZero: true
@@ -47,42 +41,33 @@ $.ajax({
                         }
                     }
                 });
-            });
+            }
         }
-    }
+    });
 
-});
+    /**
+     * Monthly Income
+     **/
+    $.ajax({
+        url: baseUrlIncome + "income/monthlyIncome", method: "GET", dataType: "json", success: function (res) {
+            console.log(res);
+            for (let i = 0; i < res.length; i++) {
+                let date = res[i][0];
+                let count = res[i][1];
+                let total = res[i][2];
+                let row = "<tr><td>" + date + "</td><td>" + count + "</td><td>" + total + "</td></tr>";
+                $("#monthlyIncomeTable").append(row);
 
-/**
- * Monthly Income
- **/
-$.ajax({
-    url: baseUrlIncome + "income/monthlyIncome",
-    method: "GET",
-    dataType: "json",
-    success: function (res) {
-        console.log(res);
-        for (let i=0; i<res.length; i++) {
-            let date = res[i][0];
-            let count = res[i][1];
-            let total = res[i][2];
-            let row = "<tr><td>" + date+ "</td><td>" + count + "</td><td>" + total + "</td></tr>";
-            $("#monthlyIncomeTable").append(row);
-
-            document.addEventListener("DOMContentLoaded", () => {
                 new Chart(document.querySelector('#monthlyIncomeChart'), {
-                    type: 'line',
-                    data: {
-                        labels: [date],
-                        datasets: [{
+                    type: 'line', data: {
+                        labels: [date], datasets: [{
                             label: 'Monthly Line Chart',
                             data: [total],
                             fill: false,
                             borderColor: 'rgb(75, 192, 192)',
                             tension: 0.1
                         }]
-                    },
-                    options: {
+                    }, options: {
                         scales: {
                             y: {
                                 beginAtZero: true
@@ -90,42 +75,34 @@ $.ajax({
                         }
                     }
                 });
-            });
+            }
         }
-    }
+    });
 
-});
+    /**
+     * Annually Income
+     **/
+    $.ajax({
+        url: baseUrlIncome + "income/AnnuallyIncome", method: "GET", dataType: "json", success: function (res) {
+            console.log(res);
+            for (let i = 0; i < res.length; i++) {
+                let date = res[i][0];
+                let count = res[i][1];
+                let total = res[i][2];
+                let row = "<tr><td>" + date + "</td><td>" + count + "</td><td>" + total + "</td></tr>";
+                $("#annuallyIncomeTable").append(row);
 
-/**
- * Annually Income
- **/
-$.ajax({
-    url: baseUrlIncome + "income/AnnuallyIncome",
-    method: "GET",
-    dataType: "json",
-    success: function (res) {
-        console.log(res);
-        for (let i=0; i<res.length; i++) {
-            let date = res[i][0];
-            let count = res[i][1];
-            let total = res[i][2];
-            let row = "<tr><td>" + date+ "</td><td>" + count + "</td><td>" + total + "</td></tr>";
-            $("#annuallyIncomeTable").append(row);
 
-            document.addEventListener("DOMContentLoaded", () => {
                 new Chart(document.querySelector('#annuallyIncomeChart'), {
-                    type: 'line',
-                    data: {
-                        labels: [date],
-                        datasets: [{
+                    type: 'line', data: {
+                        labels: [date], datasets: [{
                             label: 'Annually Line Chart',
                             data: [total],
                             fill: false,
                             borderColor: 'rgb(75, 192, 192)',
                             tension: 0.1
                         }]
-                    },
-                    options: {
+                    }, options: {
                         scales: {
                             y: {
                                 beginAtZero: true
@@ -133,8 +110,7 @@ $.ajax({
                         }
                     }
                 });
-            });
+            }
         }
-    }
-
+    });
 });
