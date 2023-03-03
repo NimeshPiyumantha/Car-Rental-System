72a631c8cbb50e482c001ce59567e6d615c5f009
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-03-03 16:53:50 +0530

    Refactor Project

--- Front_End/controller/RentController.js
+++ Front_End/controller/RentController.js
@@ -11,6 +11,7 @@ loadAllRentDetails();
 
 function loadAllRentDetails() {
     $("#rentAllDetails").empty();
+    $("#retManage").empty();
     $.ajax({
         url: RentAllManageBaseUrl + "rent/loadAllRents",
         method: "get",
@@ -18,7 +19,6 @@ function loadAllRentDetails() {
         dataType: "json",
         async: true,
         success: function (res) {
-            $("#rentAllDetails").empty();
             console.log(res.data)
             for (var i of res.data) {
                 let row = "<tr><td>" + i.rentID + "</td><td>" + i.rentDetails.at().carID + "</td><td>" + i.regUser.user_Id + "</td><td>" + i.rentDetails.at().driverID + "</td><td>" + i.requestType + "</td><td>" + i.rentType + "</td><td>" + i.pickUpDate + "</td><td>" + i.pickUpTime + "</td><td>" + i.returnTime + "</td><td>" + i.returnDate + "</td><td>" + i.location + "</td></tr>";
@@ -36,19 +36,20 @@ function loadAllRentDetails() {
 $("#search_Id").on("keypress", function (event) {
     if (event.which === 13) {
         var search = $("#search_Id").val();
-        $("#rentAllDetails").empty();
+        $("#retManage").empty();
         $.ajax({
             url: RentAllManageBaseUrl + "rent/searchDtails?search_Id=" + search,
             method: "GET",
             contentType: "application/json",
             dataType: "json",
-            success: function (res) {
-                console.log(res);
-                let row = "<tr><td>" + res.rentID + "</td><td>" + res.rentDetails.carID + "</td><td>" + res.regUser.user_Id + "</td><td>" + res.rentDetails.driverID + "</td><td>" + res.requestType + "</td><td>" + res.rentType + "</td><td>" + res.pickUpDate + "</td><td>" + res.pickUpTime + "</td><td>" + res.returnTime + "</td><td>" + res.returnDate + "</td><td>" + res.location + "</td></tr>";
-                $("#rentAllDetails").append(row);
+            success: function (i) {
+                console.log(i);
+                console.log(i.rentID)
+                let row = "<tr><td>" + i.rentID + "</td><td>" + i.rentDetails.at().carID + "</td><td>" + i.regUser.user_Id + "</td><td>" + i.rentDetails.at().driverID + "</td><td>" + i.requestType + "</td><td>" + i.rentType + "</td><td>" + i.pickUpDate + "</td><td>" + i.pickUpTime + "</td><td>" + i.returnTime + "</td><td>" + i.returnDate + "</td><td>" + i.location + "</td></tr>";
+                $("#retManage").append(row);
+                blindClickEventsRent();
             },
             error: function (error) {
-                $("#rentAllDetails").empty();
                 loadAllRentDetails();
                 let message = JSON.parse(error.responseText).message;
                 emptyMassage(message);
